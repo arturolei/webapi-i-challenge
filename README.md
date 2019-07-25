cf0d5e1fedf0d6eaa3a76ceb7af4246d6abc5068
arturolei <artie.lei@gmail.com>
2019-07-24 18:41:28 -0700

    MVP possibly complete. CRUD all working it seems

--- README.md
+++ README.md
@@ -30,8 +30,8 @@ Now that we have a way to add, update, remove and retrieve data from the provide
 
 ### Start the API and Implement Requirements
 
-- To start the server, type `npm run server` from the root folder (where the _package.json_ file is). The server is configured to restart automatically as you make changes.
-- Add the code necessary to implement the API requirements.
+- [] To start the server, type `npm run server` from the root folder (where the _package.json_ file is). The server is configured to restart automatically as you make changes.
+- [] Add the code necessary to implement the API requirements.
 - **Test the API using _Postman_ as you work through the exercises.**
 
 ### User Schema
--- index.js
+++ index.js
@@ -8,16 +8,18 @@ const server = express();
 server.use(express.json());
 
 
+//GET ALL USERS
 server.get('/api/users', (request,response)=>{
     db.find()
-    .then(hubs => {
-        response.status(200).json(hubs);
+    .then(users => {
+        response.status(200).json(users);
     })
     .catch(err =>{
-        response.status(500).json({success:false, err})
+        response.status(500).json({ error: "The users information could not be retrieved." })
     })
 })
 
+//GET USER BY ID
 server.get('/api/users/:id', (request,response)=> {
     const {id} = request.params;
     db.findById(id)
@@ -25,57 +27,76 @@ server.get('/api/users/:id', (request,response)=> {
         if(user){
             response.status(200).json(user);
         } else {
-            response.status(404).json({success:false, message: "The user with the specified ID does not exist."})
+            response.status(404).json({ message: "The user with the specified ID does not exist." })
         }
     })
+    .catch(err =>{
+        response.status(500).json({ error: "The user information could not be retrieved." })
+    })
 })
 
 
+//POST USER
 server.post('/api/users', (request,response) =>{
     const userInfo = request.body;
+    const {name, bio} = request.body;
     console.log(userInfo);
-    db.insert(userInfo)
-    .then(user => {
-        response.status(201).json({success:true,user})//201 is standard
-    })
-    .catch(err =>{
-        response.status(500).json({success:false, err})
-    });
+
+    if(!name || !bio){
+        response.status(400).json({errorMessage: "Please provide name and bio for the user." })
+    } else {
+        db.insert(userInfo)
+        .then(user => {
+            response.status(201).json(user)
+        })
+        .catch(err =>{
+            response.status(500).json({ error: "There was an error while saving the user to the database" })
+        });
+    }
+  
 })
 
-//DELETE, note the : before id to signify templatized location
-server.delete('/hubs/:id', (request, response) =>{
-    // We need something in the form of /hubs/{id} where id = id of object to be deleted
+//DELETE USER
+server.delete('/api/users/:id', (request, response) =>{
     const {id} = request.params;
     db.remove(id)
     .then(deleted => {
         if(deleted){
-            response.status(204).end(); //end method = response method that tells us to send 204 and do nothing
+            response.status(200).json({message: 'The user was deleted'})
         } else {
-            response.status(404).json({success: false, message:"I cannot find the hub you are looking for"}) //404 = DNE, does not exist
+            response.status(404).json({message: "The user with the specified ID does not exist."}) //404 = DNE, does not exist
         }
     })
     .catch(err => {
-        response.status(500).json({success:false,err});
+        response.status(500).json({ error: "The user could not be removed" });
     })
 })
 
-//UPDATE = put, note the : before id to signfy templatized location
-server.put('/hubs/:id',(request, response)=>{
+//UPDATE/put User
+server.put('/api/users/:id',(request, response)=>{
     const {id} = request.params;
-    const hubInfo = request.body;
-    console.log(hubInfo, id);
-    db.update(id, hubInfo)
-    .then(updated => {
-        if(updated){
-            response.status(200).json({success:true,updated});
-        } else {
-            response.status(404).json({success:false,message: "I cannot find the hub you were looking for"});
-        }
-    })
-    .catch(err => {
-        response.status(500).json({success:false, err}) //catch --> Catastrophic error
-    });
+    const userInfo = request.body;
+    const {name, bio} = request.body;
+    console.log(userInfo, id);
+    
+    if(!name || !bio){
+        response.status(400).json({errorMessage: "Please provide name and bio for the user." })
+    } else {
+        db.update(id, userInfo)
+        .then(updatedUser => {
+            if(updatedUser){
+                response.status(200).json(updatedUser);
+            } else {
+                response.status(404).json({message:"The user with the specified ID does not exist."});
+            }
+            
+        })
+        .catch(err =>{
+            response.status(500).json({ error: "The user information could not be modified." })
+        });
+    }
+    
+ 
 })
 
 server.listen(4000,()=>{
